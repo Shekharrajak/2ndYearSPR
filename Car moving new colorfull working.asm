e559ed64259c40c62809e1a16dbed3a77aaa7efe
shekharrajak <shekharstudy@ymail.com>
2015-03-26 20:41:47 +0530

    added

--- Car moving new colorfull working.asm
+++ Car moving new colorfull working.asm
@@ -1,7 +1,7 @@
 .model small
 .stack 64h
 .data       
-c1 db 03
+c1 db 03     ;left most wheel
 c2 db 06
 c3 db 03
 c4 db 06
@@ -15,8 +15,23 @@ main proc near
     mov ds,ax
     mov es,ax   
     
-              
-
+   ;                MOV AX,0600h ;AH=06h & AL=00h
+;    MOV BH,bgcolor ;White background (7)
+;               ;red foreground(4)
+;    MOV CX,0000h ;row 0 col 0
+;    MOV DX,184Fh ;row 24 col 79(inHex)
+;    INT 10h 
+;    
+;                 q
+                  
+                  ; mov AH, 0Ch
+;                   mov AL ,1;color of pixel
+;                  mov CX , 00h;Horizontal position of pixel
+;                  mov DX , 20h  ;     Vertical position of pixel
+;                   mov BH ,00h  ;      Display page number (graphics modes with more
+;                              ;than 1 page)  
+;                   int 10h       
+;
     
     again:  
                   
@@ -25,15 +40,18 @@ main proc near
                ;red foreground(4)
     MOV CX,0000h ;row 0 col 0
     MOV DX,184Fh ;row 24 col 79(inHex)
-    INT 10h
-    mov ah,02h
+    INT 10h 
+    
+    mov ah,02h    ;to display
     mov dl,20h
-    int 21h
+    int 21h   
+    
+    
     mov ax,0601h
-    mov bh,23h
-    mov ch,06
-    mov cl,c1
-    mov dh,07
+    mov bh,55h
+    ;mov ch,06        ;ch for row
+    mov cl,c1     ;col               ;left wheel1
+    mov dh,07       ;dh  for 
     inc c1
     mov dl,c1
     int 10h      
@@ -41,27 +59,27 @@ main proc near
     
     mov ax,0601h
     mov bh,23h
-    mov ch,06
+   ; mov ch,06
     mov cl,c2
-    mov dh,07
+    mov dh,07                            ; right wheel2
     inc c2
     mov dl,c2
     int 10h
     
     
-    mov ax,0603h
-    mov bh,71h
-    mov ch,03
+    mov ax,0603h                ;al sizeof the box
+    mov bh,71h                   ;bh color
+   ; mov ch,03                ;
     mov cl,c
     mov dh,06
-    mov dl,cc
+    mov dl,cc                                ;box middle one
     int 10h        
      
     
     mov ax,0601h
-    mov bh,23h
+    mov bh,55h
     mov ch,02
-    mov cl,c3
+    mov cl,c3                               ;left wheel2
     mov dh,03
     inc c3
     mov dl,c3
@@ -75,7 +93,7 @@ main proc near
     mov cl,c4
     mov dh,03
     inc c4
-    mov dl,c4
+    mov dl,c4                                           ;right wheel2
     int 10h       
     
     
